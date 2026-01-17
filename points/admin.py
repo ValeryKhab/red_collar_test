c69c3dd0e6c891c1f31195e8c78dcbae3bfdde49
khabibrahman.lera <khabibrahman.lera@gmail.com>
2026-01-18 00:59:07 +0700

    регистрация моделей в админке

--- points/admin.py
+++ points/admin.py
@@ -1,3 +1,52 @@
-from django.contrib import admin
+"""
+Добавление в admin
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Point, Message
+
+
+@admin.register(Point)
+class PointAdmin(admin.ModelAdmin):
+    """
+    Admin-представление для модели гео-точки
+    """
+
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "creator",
+        "latitude",
+        "longitude",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "creator",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("name", "description",)
+    
+
+@admin.register(Message)
+class MessageUserAdmin(admin.ModelAdmin):
+    """
+    Admin-представление для модели сообщения
+    """
+
+    list_display = (
+        "id",
+        "text",
+        "author",
+        "point",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "author",
+        "point",
+        "created_at",
+        "updated_at",
+    )
